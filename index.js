59c12181b1f45d16aeb1cafcf089fa6302681b1a
bp26 <aleshak260006@gmail.com>
2022-07-05 19:34:33 +0300

    Add js for navigation burger

---
+++ index.js
@@ -0,0 +1,11 @@
+const navigation_header = document.querySelector('.navigation_header');
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {
+    const expanded = navigation_header.getAttribute('data-visible');
+    if (expanded === 'false') {
+        navigation_header.setAttribute('data-visible', 'true')
+    } else if (expanded === 'true') {
+        navigation_header.setAttribute('data-visible', 'false')
+    }
+})
