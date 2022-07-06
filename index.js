d8f87c7f69b311d43d05deb43199334d59db3b67
bp26 <94074742+bp26@users.noreply.github.com>
2022-07-06 15:57:10 +0300

    feat: nav_bar closes when clicked on links

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const navigation_header = document.querySelector('.navigation_header');
-const burger = document.querySelector('.burger');
-const navLink = document.querySelector('.nav_link a');
+const navigation_header = document.querySelector(".navigation_header");
+const burger = document.querySelector(".burger");
+const link = document.querySelectorAll(".link");
 
-burger.addEventListener('click', () => {
-    const expanded = navigation_header.getAttribute('data-visible');
-    if (expanded === 'false') {
-        navigation_header.setAttribute('data-visible', 'true')
-    } else if (expanded === 'true') {
-        navigation_header.setAttribute('data-visible', 'false')
+burger.addEventListener("click", () => {
+  const expanded = navigation_header.getAttribute("data-visible");
+  if (expanded === "false") {
+    navigation_header.setAttribute("data-visible", "true");
+  } else if (expanded === "true") {
+    navigation_header.setAttribute("data-visible", "false");
+  }
+});
+
+link.forEach(function (link) {
+  link.addEventListener("click", () => {
+    const expanded = navigation_header.getAttribute("data-visible");
+    if (expanded === "true") {
+      navigation_header.setAttribute("data-visible", "false");
     }
-})
-
+  });
+});
