7c3810dedcf8dd1f2325a6ef2f897d1951e0d602
bp26 <aleshak260006@gmail.com>
2022-07-06 20:22:02 +0300

    feat: darkening div

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const navigation_header = document.querySelector(".navigation_header");
 const burger = document.querySelector(".burger");
 const link = document.querySelectorAll(".link");
+const overlay = document.querySelector('.overlay')
 
 burger.addEventListener("click", () => {
-  const expanded = navigation_header.getAttribute("data-visible");
-  if (expanded === "false") {
+const expanded = navigation_header.getAttribute("data-visible");
+if (expanded === "false") {
     navigation_header.setAttribute("data-visible", "true");
-  } else if (expanded === "true") {
+    overlay.setAttribute("data-visible", "true");
+} else if (expanded === "true") {
     navigation_header.setAttribute("data-visible", "false");
-  }
+    overlay.setAttribute("data-visible", "false");
+}
 });
 
 link.forEach(function (link) {
-  link.addEventListener("click", () => {
+link.addEventListener("click", () => {
     const expanded = navigation_header.getAttribute("data-visible");
     if (expanded === "true") {
-      navigation_header.setAttribute("data-visible", "false");
+    navigation_header.setAttribute("data-visible", "false");
+    overlay.setAttribute("data-visible", "false");
     }
-  });
 });
+});
+
+overlay.addEventListener("click", () => {
+    const expanded = navigation_header.getAttribute("data-visible");
+    if (expanded === "true") {
+        navigation_header.setAttribute("data-visible", "false");
+        overlay.setAttribute("data-visible", "false");
+    }
+    })
