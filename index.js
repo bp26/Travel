13f6d97ebf81001169cf74ef72dacdb3b79d5c99
bp26 <94074742+bp26@users.noreply.github.com>
2022-07-07 10:33:42 +0300

    feat: fixed page with navigation

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 const navigation_header = document.querySelector(".navigation_header");
 const burger = document.querySelector(".burger");
 const link = document.querySelectorAll(".link");
-const overlay = document.querySelector('.overlay')
+const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
 
 burger.addEventListener("click", () => {
-const expanded = navigation_header.getAttribute("data-visible");
-if (expanded === "false") {
+  const expanded = navigation_header.getAttribute("data-visible");
+  if (expanded === "false") {
     navigation_header.setAttribute("data-visible", "true");
     overlay.setAttribute("data-visible", "true");
-} else if (expanded === "true") {
+    body.classList.add("fixed");
+  } else if (expanded === "true") {
     navigation_header.setAttribute("data-visible", "false");
     overlay.setAttribute("data-visible", "false");
-}
+    body.classList.remove("fixed");
+  }
 });
 
 link.forEach(function (link) {
-link.addEventListener("click", () => {
+  link.addEventListener("click", () => {
     const expanded = navigation_header.getAttribute("data-visible");
     if (expanded === "true") {
-    navigation_header.setAttribute("data-visible", "false");
-    overlay.setAttribute("data-visible", "false");
+      navigation_header.setAttribute("data-visible", "false");
+      overlay.setAttribute("data-visible", "false");
+      body.classList.remove("fixed");
     }
-});
+  });
 });
 
 overlay.addEventListener("click", () => {
-    const expanded = navigation_header.getAttribute("data-visible");
-    if (expanded === "true") {
-        navigation_header.setAttribute("data-visible", "false");
-        overlay.setAttribute("data-visible", "false");
-    }
-    })
+  const expanded = navigation_header.getAttribute("data-visible");
+  if (expanded === "true") {
+    navigation_header.setAttribute("data-visible", "false");
+    overlay.setAttribute("data-visible", "false");
+    body.classList.remove("fixed");
+  }
+});
