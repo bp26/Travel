49dc495a46b12852af969ec9730f009ddb420989
bp26 <aleshak260006@gmail.com>
2022-07-08 22:24:05 +0300

    feat: cross for nav_bar done

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const navigation_header = document.querySelector(".navigation_header");
 const burger = document.querySelector(".burger");
 const link = document.querySelectorAll(".link");
 const overlay = document.querySelector(".overlay");
+const cross = document.querySelector(".cross");
 const body = document.querySelector("body");
 
 function toggleOpen() {
   navigation_header.setAttribute("toggled-open", "true");
   overlay.setAttribute("toggled-open", "true");
+  burger.setAttribute("toggled-open", "true");
+  cross.setAttribute("toggled-open", "true");
   body.classList.add("fixed");
 }
 
 function toggleClose() {
   navigation_header.setAttribute("toggled-open", "false");
   overlay.setAttribute("toggled-open", "false");
+  burger.setAttribute("toggled-open", "false");
+  cross.setAttribute("toggled-open", "false");
   body.classList.remove("fixed");
 }
 
@@ -20,10 +25,15 @@ burger.addEventListener("click", () => {
   const expanded = navigation_header.getAttribute("toggled-open");
   if (expanded === "false") {
     toggleOpen();
-  } else {
-    toggleClose();
-  }
+  } 
 });
+
+cross.addEventListener("click", () => {
+  const expanded = navigation_header.getAttribute("toggled-open");
+  if (expanded === "true") {
+    toggleClose();
+  }}
+  );
 
 link.forEach(function (link) {
   link.addEventListener("click", () => {
