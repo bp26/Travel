1c1b502751c27d4d65e3c4369ccfb8003ff3ae8c
bp26 <94074742+bp26@users.noreply.github.com>
2022-07-19 14:22:57 +0300

    feat: switch almost done

--- scripts/popup_script.js
+++ scripts/popup_script.js
@@ -4,6 +4,9 @@ const popupContent = document.querySelector(".popup_content");
 const linkAccount = document.querySelector(".link_account");
 const popupButtonLink = document.querySelector(".popup_button_link");
 const popupAccountsBlock = document.querySelector(".popup_accounts_block");
+const popupTitle = document.querySelector(".popup_title");
+const popupSignButton = document.querySelector("popup_sign_button");
+const popupBelowText = document.querySelector(".popup_changeable_text");
 
 /* Popup open-close toggle */
 
@@ -26,11 +29,30 @@ popup.addEventListener("click", (event) => {
   console.log(event);
   if (event.target.classList.contains("popup")) {
     popupOpenClose();
+    if (popupAccountsBlock.classList.contains("popup_accounts_hidden")) {
+      popupSwitchClose();
+    }
   }
 });
 
 /* Popup switch toggle */
 
-popupButtonLink.addEventListener("click", (event) => {
+function popupSwitchOpen() {
   popupAccountsBlock.classList.toggle("popup_accounts_hidden");
+  popupTitle.innerHTML = "Create account";
+  popupSignButton.innerHTML = "Sign Up";
+  popupBelowText.innerHTML = "Already have an account?";
+  popupButtonLink.innerHTML = "Log In";
+}
+
+function popupSwitchClose() {
+  popupAccountsBlock.classList.toggle("popup_accounts_hidden");
+  popupTitle.innerHTML = "Log in to your account";
+  popupSignButton.innerHTML = "Sign In";
+  popupBelowText.innerHTML = "Don't have an account?";
+  popupButtonLink.innerHTML = "Register";
+}
+
+popupButtonLink.addEventListener("click", (event) => {
+  popupSwitchOpen();
 });
