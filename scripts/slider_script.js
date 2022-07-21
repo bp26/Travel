e91561ffd9b01247c255677781e0c138b1a6c1dc
bp26 <94074742+bp26@users.noreply.github.com>
2022-07-21 11:57:46 +0300

    feat: save changes

---
+++ scripts/slider_script.js
@@ -0,0 +1,13 @@
+/* Common */
+const destinationPictures = document.querySelector(".destination_pictures");
+const destinationSliderDiv = document.querySelectorAll(
+  ".destination_slider_div"
+);
+
+/* Mobile */
+const destinationDiv1 = document.querySelector(".destination_div1");
+const destinationDiv2 = document.querySelector(".destination_div2");
+const destinationDiv3 = document.querySelector(".destination_div3");
+const previousSlideButton = document.querySelector(".previous_slide_button");
+const nextSlideButton = document.querySelector("next_slide_button");
+/* Desktop */
